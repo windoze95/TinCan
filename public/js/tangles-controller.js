2ddd5fba3ae435aeab527a5b57b6683d9cb46692
Julian <julian@localhost.localdomain>
2016-08-25 15:35:16 -0600

    asdfg

--- public/js/tangles-controller.js
+++ public/js/tangles-controller.js
@@ -1,6 +1,5 @@
 angular.module('TinCan')
     .controller('TanglesController', tangleCtrl)
-    .controller('NewTangleController', newTangleCtrl)
 
 // tangles.$inject = ['$scope', 'getLocation'];
 
@@ -23,28 +22,3 @@ function tangleCtrl($scope, getLocation) {
         //\\//\\//\\// center map in map view, detect if in pin range
     });
 };
-
-function newTangleCtrl($scope, getLocation, NewTangleFactory) {
-
-    var ntCtrl = this;
-
-    getLocation.then( function(data){
-        ntCtrl.lat = getLocation.$$state.value.lat
-        ntCtrl.lon = getLocation.$$state.value.lon
-
-        ntCtrl.newTangle = {
-            location: [lon, lat]
-        }; // eventually becomes req.body on the backend
-        ntCtrl.newTangleId;
-
-        ntCtrl.submitTangle = function() {
-            NewTangleFactory.create(ntCtrl.newTangle)
-                // .then(ntCtrl.submitSucess, ntCtrl.submitError);
-        }
-
-        ntCtrl.submitSucess = function(res) {
-            console.info('New Car Created!', res.data);
-            ntCtrl.newTangleId = res.data._id;
-        }
-    });
-}
