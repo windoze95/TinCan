777158af62e79c9d313a7a98f1e55f658e483f3b
Julian <julian@localhost.localdomain>
2016-08-28 20:38:13 -0600

    making sad faces

--- models/tangles.js
+++ models/tangles.js
@@ -1,13 +1,10 @@
-var mongoose = require('mongoose');
-
-var TangleSchema = mongoose.model('tangle', {
-
-    title:      { type: String, required: true },
-    location:   { type: Array },
-    date:       { type:Date, default:Date.now },
-    feed:       { type: String }
-
-});
+var mongoose = require('mongoose'),
+    TangleSchema = new mongoose.Schema({
+        title   : { type: String, required: true },
+        location: { type: Array },
+        date    : { type:Date, default:Date.now },
+        feed    : { type: String }
+    })
 
 // tangle.index({'location' : '2dsphere'})
 
